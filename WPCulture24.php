79d65728e90af602455dc47c49ed090c4e5cbfef
Leigh <leigh@orangeleaf.com>
2016-03-24 17:15:07 +0000

    Created base WPCulture24 object file

---
+++ WPCulture24.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * WPCulture24.php
+ *
+ * PHP Version 5.4
+ *
+ * @category
+ * @package
+ * @subpackage
+ * @author     Leigh Bicknell <leigh@orangeleaf.com>
+ * @license    Copyright Orangeleaf Systems Ltd 2013
+ * @link       http://orangeleaf.com
+ */
+
+
+/**
+ * Class WPCulture24
+ *
+ * @category
+ * @package
+ * @subpackage
+ * @author     Leigh Bicknell <leigh@orangeleaf.com>
+ * @license    Copyright Orangeleaf Systems Ltd 2013
+ * @link       http://orangeleaf.com
+ */
+class WPCulture24
+{
+
+    /**
+     * __construct
+     *
+     * @return void
+     * @access public
+     */
+    public function __construct($options)
+    {
+        // code
+    }
+}
